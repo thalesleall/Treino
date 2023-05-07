caad901ef3266de05c5fd4c1e86c6a7f0a8d180d
Thales Leal <thalesvinicius.leal@gmail.com>
2023-05-07 14:58:48 -0300

    inicio

---
+++ Jogo da velha/scripts.js
@@ -0,0 +1,50 @@
+    var pontosX = document.getElementById("px");
+    var pontosO = document.getElementById("po");
+    var spanVez = document.getElementById("vez");
+    var vez = ""
+    
+
+    pontosX.innerHTML = (`Pontos X: `);
+    pontosO.innerHTML = (`Pontos O: `);
+    spanVez.innerHTML = (`Vez do jogador = `)
+    
+    var buton1 = document.getElementById("buton1");
+    var buton2 = document.getElementById("buton2");
+    var buton3 = document.getElementById("buton3");
+    var buton4 = document.getElementById("buton4");
+    var buton5 = document.getElementById("buton5");
+    var buton6 = document.getElementById("buton6");
+    var buton7 = document.getElementById("buton7");
+    var buton8 = document.getElementById("buton8");
+    var buton9 = document.getElementById("buton9");
+    let one = document.getElementById("1");
+    let two = document.getElementById("2");
+    let three= document.getElementById("3");
+    let four = document.getElementById("4");
+    let five = document.getElementById("5");
+    let six = document.getElementById("6");
+    let seven = document.getElementById("7");
+    let eight = document.getElementById("8");
+    let nine = document.getElementById("9");
+    
+
+
+    buton1.onclick = function(){
+        one.innerHTML = ("X")}
+    buton2.onclick = function(){
+        two.innerHTML = ("X")}
+    buton3.onclick = function(){
+        three.innerHTML = ("X")}
+    buton4.onclick = function(){
+        four.innerHTML = ("X")}
+    buton5.onclick = function(){
+        five.innerHTML = ("X")}
+    buton6.onclick = function(){
+        six.innerHTML = ("X")}
+    buton7.onclick = function(){
+        seven.innerHTML = ("X")}
+    buton8.onclick = function(){
+        eight.innerHTML = ("X")}
+    buton9.onclick = function(){
+        nine.innerHTML = ("X")}
+    
